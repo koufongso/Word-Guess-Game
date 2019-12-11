619861ed1df38b0d2d60fb2dcd2824ecd156e262
Fung <koufongso@gmail.com>
2019-12-10 18:31:07 -0800

    game logic completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,80 @@
+//for word guess game
+
+//word database, each associate a img and sound
+var words = ["apple", "banana", "peach", "cat", "mouse", "horse", "car", "plane", "ship"];
+
+var maxAttempt = 11;            // defined maximum allowed guesses
+
+var winRecord = 0;              //
+var gameOver = true;            // game initial status when open the page
+var remianAttempt = maxAttempt;
+var index;                      // current word index
+var currentWord = "";           // current word
+var displayWord = [];           // "hidden" word that display on the currentWordSpan
+var inputRecord = "";           // guessed word history
+
+// connect variables to html corresponding span elements
+var winRecordSpan = document.getElementById("winRecordSpan");
+var currentWordSpan = document.getElementById("currentWordSpan");
+var remianAttemptSpan = document.getElementById("remainAttemptSpan");
+var inputRecordSpan = document.getElementById("inputRecordSpan");
+
+/////////////////////////// game logic////////////////////////////////////////
+
+//every key press trigger the event
+document.onkeypress = function (event) {
+    if (gameOver) {
+        newGame();
+    } else {
+        check(event.key.toLowerCase());
+    }
+    updateScreen() //update the html file
+};
+
+
+// start a new game, initialize all variables
+function newGame() {
+    console.log("newGame!")
+    gameOver = false;
+    remianAttempt = maxAttempt;
+    index = Math.floor(Math.random() * words.length);
+    currentWord = words[index];
+    displayWord = currentWord.split("");
+    displayWord.fill("_");
+    inputRecord="";
+    console.log("display:"+displayWord);
+}
+
+function check(input) {
+    console.log("check! input:"+input);
+    var i = currentWord.search(input);
+    if ((i != -1)) {
+        //bingo!
+        currentWord=currentWord.replace(input," ") //replace the char with " "
+        inputRecord+=input;
+        displayWord[i]=input;
+        if(!displayWord.includes("_")){
+            gameOver=true;
+            winRecord++;
+        }
+    }else{
+        //wrong!
+        if(inputRecord.search(input)==-1){
+            inputRecord+=input;
+            if((--remianAttempt)==0){
+                gameOver=true;
+            }
+        }
+    }
+}
+
+function updateScreen(){
+    winRecordSpan.innerHTML=winRecord;
+    currentWordSpan.innerHTML=displayWord.join("");
+    remianAttemptSpan.innerHTML=remianAttempt;
+    inputRecordSpan.innerHTML=inputRecord;
+}
+
+
+
 

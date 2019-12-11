1cddbb224be95f264bc420a6aebbf8e363985845
Fung <koufongso@gmail.com>
2019-12-10 23:56:34 -0800

    fix game logic, added image link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,36 +2,42 @@
 
 //word database, each associate a img and sound
 var words = ["apple", "banana", "peach", "cat", "mouse", "horse"];
-var imgs=[]
+var imgDict = {
+    "apple": "assets/images/apple.jpg",
+    "banana": "assets/images/banana.jpg",
+    "peach": "assets/images/peach.png",
+    "cat": "assets/images/cat.jpg",
+    "mouse": "assets/images/mouse.jpg",
+    "horse": "assets/images/horse.jpg"
+};
 
 var maxAttempt = 11;            // defined maximum allowed guesses
 
 var winRecord = 0;              //
-var firstTime = true;            // game initial status when open the page
+var firstTime = true;           // game initial status when open the page
 var remianAttempt = maxAttempt;
-var index;                      // current word index
-var currentWord = "";           // current word
+var index;                      // store the correct word index 
+var currentWord = [];           // remianing char 
 var displayWord = [];           // "hidden" word that display on the currentWordSpan
-var inputRecord = " ";           // guessed word history
+var inputRecord = [];          // guessed word history
 
 // connect variables to html corresponding span elements
 var winRecordSpan = document.getElementById("winRecordSpan");
 var currentWordSpan = document.getElementById("currentWordSpan");
 var remianAttemptSpan = document.getElementById("remainAttemptSpan");
 var inputRecordSpan = document.getElementById("inputRecordSpan");
+var displayImage = document.getElementById("displayImage");
 
 /////////////////////////// game logic////////////////////////////////////////
 
 //every key press trigger the event
 document.onkeypress = function (event) {
     if (firstTime) {
-        firstTime=false;
+        firstTime = false;
         newGame();
     } else {
         check(event.key.toLowerCase());
     }
-
-
     updateScreen() //update the html file
 };
 
@@ -42,42 +48,52 @@ function newGame() {
     gameOver = false;
     remianAttempt = maxAttempt;
     index = Math.floor(Math.random() * words.length);
-    currentWord = words[index];
-    displayWord = currentWord.split("");
+    currentWord = words[index].split("");
+    displayWord = words[index].split("");
     displayWord.fill("_");
-    inputRecord="";
-    console.log("display:"+displayWord);
+    inputRecord = [];
+    console.log("current:" + currentWord);
+    console.log("display:" + displayWord);
 }
 
 function check(input) {
-    console.log("check! input:"+input);
-    var i = currentWord.search(input);
-    if ((i != -1)) {
-        //bingo!
-        currentWord=currentWord.replace(input,"\n") //replace the char with " "
-        inputRecord+=input;
-        displayWord[i]=input;
+    console.log("check! input:" + input);
+    console.log("currentword:" + currentWord.toString(""));
+    console.log("displayword:" + displayWord.toString(""));
 
-        if(!displayWord.includes("_")){
+    var i = currentWord.indexOf(input);
+    console.log("i:" + i);
+    if ((i != -1)) {
+        //hit!
+        currentWord[i]="";        //replace the char by ""
+        inputRecord.push(input);     //add it to the record
+        displayWord[i] = input;   //reveal its position
+
+        console.log("displayword:" + displayWord.join(""));
+        if (displayWord.join("") == words[index]) {
+            //bingo!
+            displayImage.src=imgDict[words[index]];
             winRecord++;
             newGame();
         }
-    }else{
+    } else {
         //wrong!
-        if(inputRecord.search(input)==-1){
-            inputRecord+=input;
-            if((--remianAttempt)==0){
+        if (inputRecord.indexOf(input) == -1) {
+            inputRecord.push(input);
+            if ((--remianAttempt) == 0) {
                 newGame();
             }
         }
     }
+
+    console.log("inputRecord:" + inputRecord);
 }
 
-function updateScreen(){
-    winRecordSpan.innerHTML=winRecord;
-    currentWordSpan.innerHTML=displayWord.join("");
-    remianAttemptSpan.innerHTML=remianAttempt;
-    inputRecordSpan.innerHTML=inputRecord;
+function updateScreen() {
+    winRecordSpan.innerHTML = winRecord;
+    currentWordSpan.innerHTML = displayWord.join("");
+    remianAttemptSpan.innerHTML = remianAttempt;
+    inputRecordSpan.innerHTML = inputRecord.toString();
 }
 
 

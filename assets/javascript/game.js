74ba7e63691b19b6315478e683ef4fb319b675c4
Fung <koufongso@gmail.com>
2019-12-11 12:16:24 -0800

    wrap wordGame elements into an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,116 +1,119 @@
-/*---------------------------------- data ---------------------------------------------*/
-//word database, each associate a img and sound
-var dataBase = {
-    "apple": "assets/images/apple.jpg",
-    "banana": "assets/images/banana.jpg",
-    "peach": "assets/images/peach.png",
-    "cat": "assets/images/cat.jpg",
-    "mouse": "assets/images/mouse.jpg",
-    "horse": "assets/images/horse.jpg"
-};
+var wordGame = {
+    /*---------------------------------- data ---------------------------------------------*/
+    //word database, each associate a img and sound
+    dataBase: {
+        apple: "assets/images/apple.jpg",
+        banana: "assets/images/banana.jpg",
+        peach: "assets/images/peach.png",
+        cat: "assets/images/cat.jpg",
+        mouse: "assets/images/mouse.jpg",
+        horse: "assets/images/horse.jpg"
+    },
 
-/*---------------------------------- setting ---------------------------------------------*/
-var maxAttempt = 11;            // defined maximum allowed guesses
+    /*---------------------------------- const variables ---------------------------------------------*/
+    maxAttempt: 11,                 // defined maximum allowed guesses
 
-/*---------------------------------- variables -------------------------------------------*/
-var winRecord = 0;              //
-var firstTime = true;           // game initial status when open the page
-var remianAttempt = maxAttempt;
-var index=-1;                   // store the correct word index 
-var key;                        // store the word 
+    /*---------------------------------- variables -------------------------------------------*/
+    winRecord: 0,                   // initial winRecord
+    firstTime: true,                // game initial status when open the page
+    remianAttempt: this.maxAttempt,
+    index: -1,                      // store the correct word index 
+    key: -2,                        // store the word 
 
-var currentWord = [];           // remianing char 
-var displayWord = [];           // "hidden" word that display on the currentWordSpan
-var inputRecord = [];           // guessed word history
+    currentWord: [],                // remianing char 
+    displayWord: [],                // "hidden" word that display on the currentWordSpan
+    inputRecord: [],                // guessed word history
 
-// connect variables to html corresponding span elements
-var winRecordSpan = document.getElementById("winRecordSpan");
-var currentWordSpan = document.getElementById("currentWordSpan");
-var remianAttemptSpan = document.getElementById("remainAttemptSpan");
-var inputRecordSpan = document.getElementById("inputRecordSpan");
-var displayImage = document.getElementById("displayImage");
+    // connect variables to html corresponding span elements
+    winRecordSpan: document.getElementById("winRecordSpan"),
+    currentWordSpan: document.getElementById("currentWordSpan"),
+    remianAttemptSpan: document.getElementById("remainAttemptSpan"),
+    inputRecordSpan: document.getElementById("inputRecordSpan"),
+    displayImage: document.getElementById("displayImage"),
 
-/*---------------------------------- logic -------------------------------------------*/
+    /*---------------------------------- methods -------------------------------------------*/
+    isFirstTime() {
+        return this.firstTime;
+    },
+
+    // start a new game, initialize all variables
+    newGame() {
+        this.firstTime = false;
+        console.log("newGame!")
+        this.remianAttempt = this.maxAttempt;
+
+        //not to generate the previous word
+        do {
+            var temp = Math.floor(Math.random() * (Object.keys(this.dataBase).length));
+        } while (temp == this.index);
+        this.index = temp;
+        this.key = Object.keys(this.dataBase)[this.index];
+        this.currentWord = this.key.split("");
+        this.displayWord = this.key.split("");
+        this.displayWord.fill("_");
+        this.inputRecord = [];
+
+        console.log("index:" + this.index);
+        console.log("key:" + this.key);
+        console.log("current:" + this.currentWord);
+        console.log("display:" + this.displayWord);
+    },
+
+    check(input) {
+        var i = this.currentWord.indexOf(input); //check coresponding char index
+
+        if ((i != -1)) {
+            //hit!
+            this.currentWord[i] = "";        //replace the char by ""
+            this.inputRecord.push(input);  //add it to the record
+            this.displayWord[i] = input;   //reveal its position
+            this.remianAttempt--;
+
+            console.log("displayword:" + this.displayWord.join(""));
+            if (this.displayWord.join("") == this.key) {
+                //bingo!
+                this.displayImage.src = this.dataBase[this.key];
+                this.winRecord++;
+                new Audio("assets/sounds/correct.wav").play(); //play correct sound effect
+                this.newGame();
+            }
+        } else {
+            //wrong!
+            if (this.inputRecord.indexOf(input) == -1) {
+                this.inputRecord.push(input);
+                if ((--this.remianAttempt) == 0) {
+                    //fail!
+                    new Audio("assets/sounds/wrong.mp3").play();
+                    this.newGame();
+                }
+            }
+        }
+
+        console.log("check! input:" + input);
+        console.log("i:" + i);
+        console.log("currentword:" + this.currentWord.toString(""));
+        console.log("displayword:" + this.displayWord.toString(""));
+        console.log("inputRecord:" + this.inputRecord);
+    },
+
+    updateScreen() {
+        this.winRecordSpan.innerHTML = this.winRecord;
+        this.currentWordSpan.innerHTML = this.displayWord.join("");
+        this.remianAttemptSpan.innerHTML = this.remianAttempt;
+        this.inputRecordSpan.innerHTML = this.inputRecord.toString();
+    }
+}
+
 
 //every key press trigger the event
 document.onkeypress = function (event) {
-    if (firstTime) {
-        firstTime = false;
-        newGame();
+    if (wordGame.isFirstTime()) {
+        wordGame.newGame();
     } else {
-        check(event.key.toLowerCase());
+        wordGame.check(event.key.toLowerCase());
     }
-    updateScreen() //update the html file
+    wordGame.updateScreen() //update the html file
 };
-
-
-// start a new game, initialize all variables
-function newGame() {
-    console.log("newGame!")
-    gameOver = false;
-    remianAttempt = maxAttempt;
-
-    //not to generate the previous word
-    do{
-        var temp = Math.floor(Math.random() * (Object.keys(dataBase).length));
-    }while(temp==index);
-    index = temp;
-    console.log("index:"+index);
-    
-    key = Object.keys(dataBase)[index];
-    console.log("key:"+key);
-
-    currentWord = key.split("");
-    displayWord = key.split("");
-    displayWord.fill("_");
-    inputRecord = [];
-    console.log("current:" + currentWord);
-    console.log("display:" + displayWord);
-}
-
-function check(input) {
-    console.log("check! input:" + input);
-    console.log("currentword:" + currentWord.toString(""));
-    console.log("displayword:" + displayWord.toString(""));
-
-    var i = currentWord.indexOf(input); //check coresponding char index
-    console.log("i:" + i);
-    if ((i != -1)) {
-        //hit!
-        currentWord[i]="";        //replace the char by ""
-        inputRecord.push(input);  //add it to the record
-        displayWord[i] = input;   //reveal its position
-        remianAttempt--;
-
-        console.log("displayword:" + displayWord.join(""));
-        if (displayWord.join("") == key) {
-            //bingo!
-            displayImage.src=dataBase[key];
-            winRecord++;
-            new Audio("assets/sounds/correct.wav").play(); //play correct sound effect
-            newGame();
-        }
-    } else {
-        //wrong!
-        if (inputRecord.indexOf(input) == -1) {
-            inputRecord.push(input);
-            if ((--remianAttempt) == 0) {
-                //fail!
-                new Audio("assets/sounds/wrong.mp3").play();
-                newGame();
-            }
-        }
-    }
-
-    console.log("inputRecord:" + inputRecord);
-}
-
-function updateScreen() {
-    winRecordSpan.innerHTML = winRecord;
-    currentWordSpan.innerHTML = displayWord.join("");
-    remianAttemptSpan.innerHTML = remianAttempt;
-    inputRecordSpan.innerHTML = inputRecord.toString();
-}
 
 
 
